504630765c9d70ba9f51e1d0516359f6b1b7f79d
tombroucke <tom@tombroucke.be>
2024-02-28 10:50:18 +0100

    fix aria-controls

--- publishes/resources/views/components/trigger/offcanvas.blade.php
+++ publishes/resources/views/components/trigger/offcanvas.blade.php
@@ -1,3 +1,3 @@
-<x-button {{ $attributes }} theme="{{ $theme ?? 'primary' }}" type="button" data-bs-toggle="offcanvas" data-bs-target="{{ $target }}" aria-controls="{{ $target }}">
+<x-button {{ $attributes }} theme="{{ $theme ?? 'primary' }}" type="button" data-bs-toggle="offcanvas" data-bs-target="{{ $target }}" aria-controls="{{ str_replace('#', '', $target) }}">
   {{ $slot }}
 </x-button>
